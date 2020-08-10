51e9107005ba68593ef8bd59956bd58179ce29e0
royartarian <royartarian@gmail.com>
2020-08-10 12:02:13 -0400

    removed favicon

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ const VoiceRSS = {
 // Passing Joke to VoiceRSS API
 function tellMe(joke) {
   VoiceRSS.speech({
-    key: config.API_KEY,
+    key: 'dfb831372a3949b7826f479096295196',
     src: joke,
     hl: "en-us",
     v: "Linda",
